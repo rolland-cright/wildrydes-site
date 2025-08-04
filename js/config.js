cd33bdfa146cd930a40b31b00ed8eb0fbbcdc1b5
rolland-cright <rolland.cright@javanun.com>
2025-08-04 17:58:24 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_KarRQiR5x', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3kmd6heu09q6tg701keppqsbdj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_xbPcCQbLw', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6tkuj09figpon2dlb5oijok6tk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
